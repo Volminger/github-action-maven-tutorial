cab7a172b9cb9b1fce6e6fe9fc72b0739e60678d
Volminger <Alexander_volminger@live.se>
2020-04-26 22:25:17 +0200

    Correct test

--- src/test/java/TestDeepThought.java
+++ src/test/java/TestDeepThought.java
@@ -11,7 +11,7 @@ public class TestDeepThought {
         @DisplayName("Test the answer to everything")
         void testAnswerToAll(){
             DeepThought superComputer = new DeepThought();
-            assertEquals( 41 , superComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything());
+            assertEquals( 42 , superComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything());
         }
 
 }
