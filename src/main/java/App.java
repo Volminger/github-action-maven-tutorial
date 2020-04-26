176a6c6c7688df8d893bd3cd463a5428a9567a2a
Volminger <Alexander_volminger@live.se>
2020-04-26 20:42:43 +0200

    Removed instance of class1.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,7 +4,6 @@ public class App
     {
         System.out.println("I am your awesome Java application!");
 
-        System.out.println("Creating Class1 Object");
-        Class1 object = new Class1(24, "What is the answer to life");
+        
     }
 }
