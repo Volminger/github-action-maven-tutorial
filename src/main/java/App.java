9ed8d0274e8f4756f96179b22d422bcbfbd2e3a4
Volminger <Alexander_volminger@live.se>
2020-04-26 19:44:18 +0200

    Added a instance of Class1 to main

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello World!");
+        System.out.println("I am your awesome Java application!");
+
+        System.out.println("Creating Class1 Object");
+        Class1 object = new Class1(24, "What is the answer to life");
     }
 }
