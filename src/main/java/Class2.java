62441a5b50491bb262d2fbc93328784078e94631
Volminger <Alexander_volminger@live.se>
2020-04-26 19:39:58 +0200

    Init changes to Class2

--- src/main/java/Class2.java
+++ src/main/java/Class2.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Class2 {
+    private String name;
+
+    public Class2(String input){
+        this.name   = input;
+    }
+
+    public String get_name(){
+        return this.name;
+    }
+
+    public void set_name(String input){
+        this.name = input;
+    }
+
 }
