ed4310e01a04c7aeb6c5a909ca029955a62788eb
Volminger <Alexander_volminger@live.se>
2020-04-26 22:14:51 +0200

    Fail tests

--- src/test/java/TestDeepThought.java
+++ src/test/java/TestDeepThought.java
@@ -1,5 +1,6 @@
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
 public class TestDeepThought {
@@ -10,7 +11,7 @@ public class TestDeepThought {
         @DisplayName("Test the answer to everything")
         void testAnswerToAll(){
             DeepThought superComputer = new DeepThought();
-            assertEquals( 42 , superComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything());
+            assertEquals( 41 , superComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything());
         }
 
 }
