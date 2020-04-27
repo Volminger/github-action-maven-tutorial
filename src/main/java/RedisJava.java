421da4cb1f826e81378b75f4afe88a8d10267ed1
Volminger <Alexander_volminger@live.se>
2020-04-27 22:33:24 +0200

    more redis code

--- src/main/java/RedisJava.java
+++ src/main/java/RedisJava.java
@@ -5,7 +5,9 @@ public class RedisJava {
         //Connecting to Redis server on localhost
         Jedis jedis = new Jedis("localhost");
         System.out.println("Connection to server sucessfully");
-        //check whether server is running or not
-        System.out.println("Server is running: "+jedis.ping());
+        //set the data in redis string
+        jedis.set("tutorial-name", "Redis tutorial");
+        // Get the stored data and print it
+        System.out.println("Stored string in redis:: "+ jedis.get("tutorial-name"));
     }
 }
